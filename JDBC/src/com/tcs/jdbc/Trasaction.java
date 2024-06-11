a43bc1f21df865d0a11af333ce840ee7acc0cdaa
GPSKISHORE <gpskishore93@gmail.com>
2024-06-11 10:44:00 +0530

    Updated Transaction code

--- JDBC/src/com/tcs/jdbc/Trasaction.java
+++ JDBC/src/com/tcs/jdbc/Trasaction.java
@@ -10,54 +10,55 @@ import java.util.Scanner;
 public class Trasaction {
 
 	public static void main(String[] args) {
-		try(Scanner sc=new Scanner(System.in);
-			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Kishore@123");
-			PreparedStatement ps=con.prepareStatement("select sal from emp where empno= ?");
-			Statement st=con.createStatement()){
-			
+		try (Scanner sc = new Scanner(System.in);
+				Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system",
+						"Kishore@123");
+				PreparedStatement ps = con.prepareStatement("select sal from emp where empno= ?");
+				Statement st = con.createStatement()) {
+
 			con.setAutoCommit(false);
 			System.out.print("Please enter EMP no that you want to dept ");
-			int dep=sc.nextInt();
-			
+			int dep = sc.nextInt();
+
 			System.out.print("Please enter EMP no that you want to credit ");
-			int cre=sc.nextInt();
-			
+			int cre = sc.nextInt();
+
 			System.out.print("Please enter Amount ");
-			int amo=sc.nextInt();
-			
-			st.addBatch("UPDATE EMP SET SAL= SAL - "+amo+" WHERE EMPNO= "+dep);
-			st.addBatch("UPDATE EMP SET SAL= SAL + "+amo+" WHERE EMPNO= "+cre);
-			
+			int amo = sc.nextInt();
+
+			st.addBatch("UPDATE EMP SET SAL= SAL - " + amo + " WHERE EMPNO= " + dep);
+			st.addBatch("UPDATE EMP SET SAL= SAL + " + amo + " WHERE EMPNO= " + cre);
+
 			ps.setInt(1, dep);
-			
-			java.sql.ResultSet rs=ps.executeQuery();
+
+			java.sql.ResultSet rs = ps.executeQuery();
 			rs.next();
-			
-			int balence=rs.getInt(1);
-			
-			if(amo>balence) {
+
+			int balence = rs.getInt(1);
+
+			if (amo > balence) {
 				System.out.println("Inssueficient funds ");
 				return;
 			}
-			
-			int []res=st.executeBatch();
-			boolean tra=true;
-			for(int i=0;i<res.length;i++) {
-				if(res[i]==0) {
-					tra=false;
+
+			int[] res = st.executeBatch();
+			boolean tra = true;
+			for (int i = 0; i < res.length; i++) {
+				if (res[i] == 0) {
+					tra = false;
 					break;
 				}
 			}
-			if(tra) {
+			if (tra) {
 				System.out.println("Transaction succesfull and commited ");
 				con.commit();
-			}else {
+			} else {
 				System.out.println("Transaction failed");
 				con.rollback();
 			}
-		}catch(SQLException se) {
+		} catch (SQLException se) {
 			se.printStackTrace();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
